c15e0fd507dd094ede8a827f43893cbe7933c6d6
Sebastian <sebamedina273@gmail.com>
2025-04-18 13:25:48 -0400

    Mi primer commit

---
+++ HolaMundo.js
@@ -0,0 +1,28 @@
+let num = 2;//Funciona como una variable que puede cambiar su contenido
+const nombre = "Sebastian";//Permanece igual siempre
+var edad = 21;
+
+// ? cammelCase = primerApellido
+// ? PascalCase = SegundoApellIdo
+// ? snake_case = sebastian_medina
+// ? kebap-case / lisp-case = primer-nombre
+
+console.log(num);
+console.log(nombre);
+console.log(edad);
+
+// ! Tipos de datos primirtivos
+const numero = 1;
+
+const infinito = -1 / 0;
+const nan = "Esto es un numero" / 2;
+
+//console.log(infinito);
+//console.log(nan);
+
+//const largeNumber = 023909909340n + 10n;
+//console.log(largeNumber);
+
+const string1 = "Esto es un string";
+const string2 = 'Esto tambien es un string';
+const string3 = 'Hola, mi nomhbre es ${nombre}';
